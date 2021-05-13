db637277e26c63575d66455ed90b27b84ae87a6f
Carlos Cristobal <crist074@umn.edu>
2021-05-13 15:11:39 -0600

    Update APITests.cs

--- tests/APITests.cs
+++ tests/APITests.cs
@@ -21,6 +21,7 @@ namespace Automation
         [ThreadStatic] private static ExtentTest test;
         private static ExtentReports extent;
         [ThreadStatic] private bool testPassed;
+        static string currentDirectory = $"{Environment.CurrentDirectory}\\..\\..\\..";
 
         [SetUp]
         public void Setup(){
@@ -89,7 +90,7 @@ namespace Automation
     
         public static IEnumerable<APITestCaseSource> GetTestData(){
             List<APITestCaseSource> sources = new List<APITestCaseSource>();
-            using (var reader = new StreamReader(@"E:\Github\docker-automation\tests\apitestcasesources.csv")){
+            using (var reader = new StreamReader(@$"{currentDirectory}\apitestcasesources.csv")){
                 using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture)){
                     var records = csv.GetRecords<APITestCaseSource>();
                     foreach (APITestCaseSource record in records){
